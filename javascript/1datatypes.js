95b1011cc4c1be649b43f079398ff93dada3ed7d
armanahad <armanahad6207@gmail.com>
2023-07-24 08:33:45 +0530

    add java script and other properties

---
+++ javascript/1datatypes.js
@@ -0,0 +1,90 @@
+// astring a="hii","armanahad","aditya"
+// number a=12345678 1223.4545
+// boolean a=true false
+
+
+// Es5
+// var a =>decelaration
+// a="heelo world"=>assignment
+var a="hello world";
+var b=123;
+var c=true;
+console.log(typeof(a))//string
+console.log(typeof(b))//number
+console.log(typeof(c))//boolean
+
+var a=23.56;
+console.log(typeof(a))// number
+var a=10;
+var b=5;
+console.log(a+b);//15
+console.log(a%b);//0
+console.log(a*b);//50
+console.log(a/b);//2
+
+// 5%2 =>3
+// 5%3 =>2
+// 5%4 =>1
+// 5%5 =>0
+
+// 2%3 =>0
+// 3%5 =>0
+ var a="hello"
+ var b="world"
+//  "helo"+"world"
+ //concatenate
+ var c=a+b;
+ console.log(a+b);
+ console.log(a-b)// NaN => not a number
+    
+
+//  string+string =>  string
+//  string+number =>  string
+//  number+string =>  string
+// number+number  =>   number
+
+
+
+// for example
+ 
+// 1+"2"+3+4 =>1234 string
+// 1+2+"3"+4 =>1234 string
+// 1+2+3+"4" =>1234 string
+// 1+2+3+4 =>10 string
+
+ var b="10"+20+30
+ console.log(b)//102030
+ var b=1+2+3+4
+ console.log(b)//10
+
+//  "10"-1 =>9
+//   1-"10" =>-9
+//  "10a"-1 =>NaN not a number
+
+ console.log("10"-1)//9
+
+ console.log(1-"10")//-9
+  
+ console.log("10a"-1) //Nan => not a number
+
+//  "arman"+"ahad"=>armanahad +means cocatenate
+//  "arman"-"ahad"=>NaN not a Number
+//  "10"-1        => 9 -means subtraction
+
+// true =>1
+// false =>0
+
+// true+ true 
+// 1   +  1 => 2
+//  "true" +"true" =>truetrue
+
+ 
+
+  console.log(true+true) //2
+//   true+false
+//   1   +   0 =>1
+console.log(true+false)//1
+
+console.log(true+"false")//truefalse
+console.log(true-"true")// NaN =>not a number
+
